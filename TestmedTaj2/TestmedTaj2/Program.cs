bd049c3a184c8a510101494b3bc4e8146326e3ac
Tobias Enkebølle <tobiasenkebolle@gmail.com>
2024-10-31 13:32:14 +0100

    Branch test til Taj

--- TestmedTaj2/TestmedTaj2/Program.cs
+++ TestmedTaj2/TestmedTaj2/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Taj fra Tobias");
         }
     }
 }
